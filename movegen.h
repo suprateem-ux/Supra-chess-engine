3caf800924335cbb3323937e34cb2a8a9eb7bb3c
suprateem-ux <supraontwitch708@gmail.com>
2025-07-19 15:42:04 +0530

    Update movegen.h

--- movegen.h
+++ movegen.h
@@ -1,22 +1,30 @@
 #pragma once
-#include <string>
+#include "bitboard.h"
+#include "square.h"
 #include <vector>
+#include <string>
 
-enum Color { WHITE, BLACK };
-enum PieceType { PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING };
+enum MoveType { NORMAL, CAPTURE, PROMOTION, EN_PASSANT, CASTLING };
 
-struct Move {
-    int from_sq, to_sq;
-    int promotion = 0;
-    bool is_capture = false;
-
+class Move {
+    int data;
+public:
+    Move() : data(0) {}
+    Move(int from, int to, int promo = 0, MoveType type = NORMAL);
+    int from() const;
+    int to() const;
+    int promotion() const;
+    MoveType type() const;
     std::string to_string() const;
-    int from() const { return from_sq; }
-    int to() const { return to_sq; }
+    bool operator==(const Move& other) const { return data == other.data; }
+    bool operator!=(const Move& other) const { return !(*this == other); }
 };
 
-namespace Position {
-    void parse_position(const std::string& cmd);
-    Color side_to_move();
-    std::vector<Move> generate_legal_moves();
-}
+struct Position {
+    void set_fen(const std::string& fen);
+    static void parse_position(const std::string& cmd);
+    std::vector<Move> generate_moves() const;
+    void make_move(const Move& move);
+    void undo_move();
+    int side_to_move() const;
+};
