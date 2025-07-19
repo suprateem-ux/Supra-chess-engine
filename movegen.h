26a1fa1bc9ebeb62a6d895c2386a94fe30ca8c49
suprateem-ux <supraontwitch708@gmail.com>
2025-07-19 14:10:44 +0530

    Update movegen.h

--- movegen.h
+++ movegen.h
@@ -1,19 +1,15 @@
 #pragma once
-#include <vector>
 #include <string>
 
-enum Color { WHITE, BLACK };
-enum Piece { NONE, PAWN, KNIGHT, BISHOP, ROOK, QUEEN, KING };
-
 struct Move {
-    int from_sq, to_sq;
-    Piece promotion = NONE;
-    bool is_capture = false;
+    int from() const { return 0; }
+    int to() const { return 0; }
 
-    Move(int from = 0, int to = 0, Piece promo = NONE, bool cap = false)
-        : from_sq(from), to_sq(to), promotion(promo), is_capture(cap) {}
+    std::string to_string() const {
+        return "e2e4"; // Stub
+    }
 
-    std::string to_string() const;
-    int from() const { return from_sq; }
-    int to() const { return to_sq; }
+    bool operator!=(const Move& other) const {
+        return to() != other.to(); // Basic example
+    }
 };
