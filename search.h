5da4f8e6c713875253a55b33d520879b4e83b024
suprateem-ux <supraontwitch708@gmail.com>
2025-07-19 15:42:26 +0530

    Update search.h

--- search.h
+++ search.h
@@ -1,14 +1,10 @@
 #pragma once
-#include <string>
 #include "movegen.h"
-
-constexpr int MAX_DEPTH = 64;
-constexpr int INF = 100000;
-using Score = int;
+#include <string>
 
 namespace Search {
+    constexpr int MAX_DEPTH = 64;
+    constexpr int INF = 100000;
     void start_search(const std::string& cmd);
     void set_threads(int n);
-    void update_killer(int ply, Move move);
-    void update_history(Color side, Move move, int depth);
 }
