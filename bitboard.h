5ff7cf6717eb6258f2a25df3d712625ef67afbdc
suprateem-ux <supraontwitch708@gmail.com>
2025-07-19 15:41:25 +0530

    Update bitboard.h

--- bitboard.h
+++ bitboard.h
@@ -5,9 +5,11 @@ using Bitboard = uint64_t;
 
 extern Bitboard RANK_MASKS[8];
 extern Bitboard FILE_MASKS[8];
+extern Bitboard SQUARE_BB[64];
+extern const char* SQUARE_NAMES[64];
+
+inline int popcount(Bitboard bb) { return __builtin_popcountll(bb); }
+inline int lsb(Bitboard bb) { return __builtin_ctzll(bb); }
+inline int msb(Bitboard bb) { return 63 - __builtin_clzll(bb); }
 
 void init_bitboards();
-
-inline int popcount(Bitboard bb);
-inline int lsb(Bitboard bb);
-inline int msb(Bitboard bb);
