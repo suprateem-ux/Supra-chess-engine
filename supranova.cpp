44357601bb3400cc6841a90c839a167ae8e7d4be
suprateem-ux <supraontwitch708@gmail.com>
2025-07-19 23:57:14 +0530

    Update supranova.cpp

--- supranova.cpp
+++ supranova.cpp
@@ -18,7 +18,7 @@
 #include "syzygy.h"
 #include "polyglot.h"
 #include "time.h"
-
+class Position;
 const char* SquareNames[64] = {
     "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1",
     "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2",
