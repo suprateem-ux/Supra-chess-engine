f17e407bb501cc9f1151fa197a0399c8249df727
suprateem-ux <supraontwitch708@gmail.com>
2025-07-19 15:44:06 +0530

    Update polyglot.h

--- polyglot.h
+++ polyglot.h
@@ -1,8 +1,6 @@
 #pragma once
 #include <string>
-#include "movegen.h"
 
 namespace Polyglot {
-    void load(const std::string& file);
-    Move get_book_move(const struct Position& pos);
+    void load(const std::string& path);
 }
