21b62cee3054c528f32f3c4dc2bfa9f0ac4e9573
suprateem-ux <supraontwitch708@gmail.com>
2025-07-19 15:37:51 +0530

    Create time.h

---
+++ time.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <chrono>
+
+namespace TimeManager {
+    extern int moveTime;
+    extern int increment;
+    extern int timeLeft;
+    extern int movesToGo;
+    extern std::chrono::steady_clock::time_point startTime;
+
+    void set_time_control(const std::string& cmd, bool isWhite);
+    void start_timer();
+    int time_remaining();
+    bool time_up();
+}
